83fa816556244b014c23bb6126b1a91c3f22fb2d
gquadrati <greta.quadrati@pagopa.it>
2022-05-19 21:00:57 +0200

    useless change

--- Dangerfile.ts
+++ Dangerfile.ts
@@ -1,7 +1,8 @@
 // import custom DangerJS rules
 // see http://danger.systems/js
 // see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
-// tslint:disable-next-line:prettier
+// tslint:disable-next-line:
+
 import checkDangers from 'danger-plugin-digitalcitizenship';
 
 checkDangers();
