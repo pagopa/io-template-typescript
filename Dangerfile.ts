761dda5d735540a097c96e16f9701d75d14b056a
Garma <64153597+Garma00@users.noreply.github.com>
2023-01-30 11:42:42 +0100

    [#ICC-321] remove danger (#30)
    * remove Danger.ts
    
    * remove danger from pipeline
    
    * remove danger packages
    
    * add github action to validate pr title
    
    * remove danger ref from .eslintrc

--- Dangerfile.ts
+++
@@ -1,6 +0,0 @@
-// import custom DangerJS rules
-// see http://danger.systems/js
-// see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
-import checkDangers from '@pagopa/danger-custom-rules';
-
-checkDangers();
